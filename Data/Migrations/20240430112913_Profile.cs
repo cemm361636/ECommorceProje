b1ab8fa17bb10d677acab7c590bd009ec1098015
cemkaygısız <149940447+cemm361636@users.noreply.github.com>
2024-04-30 15:05:10 +0300

    Edit

---
+++ Data/Migrations/20240430112913_Profile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Profile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreateDate",
+                value: new DateTime(2024, 4, 30, 14, 29, 12, 537, DateTimeKind.Local).AddTicks(801));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreateDate",
+                value: new DateTime(2024, 4, 30, 0, 43, 16, 664, DateTimeKind.Local).AddTicks(8262));
+        }
+    }
+}
